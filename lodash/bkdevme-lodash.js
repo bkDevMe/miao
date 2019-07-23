432b480e0ff348cec4aec110711ef133be4ef8aa
bkDevMe <bkflying1@163.com>
2019-07-23 19:54:02 +0800

    提交

--- lodash/bkdevme-lodash.js
+++ lodash/bkdevme-lodash.js
@@ -4,21 +4,13 @@ var bkdevme = {
     },
     chunk: function (ary, size = 1) {
         if (ary.length == 0) return []
-        let result = []
         let start = 0
+        let resIndex = 0
+        const result = new Array(Math.ceil(ary.length / size))
+
         while (start < ary.length) {
-            let temp = []
-            if (start + size > ary.length) break
-            for (let i = start; i < start + size; i++)
-                temp.push(ary[i])
-            result.push(temp)
-            start = start + size
+            result[resIndex++] = ary.slice(start, start += size)
         }
-        let temp = []
-        for (let i = start; i < ary.length; i++) {
-            temp.push(ary[i])
-        }
-        result.push(temp)
         return result
     }
 }
